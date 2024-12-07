409918f6b46a243cace2d2eb06ce018b67bcd945
Sohaib <sohaib.ouakani@studio.unibo.it>
2024-12-07 14:03:51 +0100

    reactivegui02 functionality completed, missing javadoc and style check

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -14,4 +14,75 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class ConcurrentGUI extends JFrame {
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    final JLabel label = new JLabel();
+
+    public ConcurrentGUI () {
+        super();
+        final Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenDimension.getWidth() * WIDTH_PERC), (int) (screenDimension.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JPanel canvas = new JPanel();
+        final JButton stop = new JButton("Stop");
+        final JButton up = new JButton("Up");
+        final JButton down = new JButton("Down");
+
+        canvas.add(label);
+        canvas.add(up);
+        canvas.add(down);
+        canvas.add(stop);
+
+        this.getContentPane().add(canvas);
+        this.setVisible(true);
+
+        final Agent agent = new Agent();
+        
+        stop.addActionListener(a -> {
+            up.setEnabled(false);
+            down.setEnabled(false);
+            agent.stopCounting();
+        });
+        up.addActionListener(a -> agent.countingUp());
+        down.addActionListener(a -> agent.countingDown());
+
+        new Thread(agent).start();
+    }
+
+    private class Agent implements Runnable {
+        private int counter;
+        private volatile boolean stop = false;
+        private volatile boolean up = true;
+
+        @Override
+        public void run() {
+            try {
+                while (!this.stop) {
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeLater(() -> ConcurrentGUI.this.label.setText(nextText));
+                    if (this.up) {
+                        this.counter++;
+                    } else {
+                        this.counter--;
+                    }
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void countingUp() {
+            this.up = true;
+        }
+
+        public void countingDown() {
+            this.up = false;
+        }
+    }
 }
